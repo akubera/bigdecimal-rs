2c418a7c0eb5a7e77c132a268f187dbf2473b39a
Andrew Kubera <andrewkubera@gmail.com>
2020-01-09 16:42:14 -0500

    example:repeated_squares: Reduce iterations to 26

--- examples/repeated_squares.rs
+++ examples/repeated_squares.rs
@@ -10,7 +10,7 @@ fn main()
   //   let mut x = BigDecimal::from(1.1);
     let mut x = BigDecimal::from_str("1.1").unwrap();
     // for iter in 0..1_000_000 {
-    for iter in 0..1_000 {
+    for iter in 0..26 {
         let start = Instant::now();
         x = x.clone() * x;
         // x = x.take_and_square();
@@ -19,5 +19,3 @@ fn main()
         println!("iter {} takes {} secs", iter, usage.as_secs() as f32 + usage.subsec_nanos() as f32 / 1.0e9);
     }
 }
-
-
