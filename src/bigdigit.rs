99aa6dc69cbbdcfbec1e60b40238693ab1033f0f
Andrew Kubera <andrewkubera@gmail.com>
2022-06-26 02:33:26 -0400

    Implement FromIterator for BigDigitVec

--- src/bigdigit.rs
+++ src/bigdigit.rs
@@ -5,7 +5,13 @@
 //! These are distinct from the BigDigits in BigIntger
 //!
 
+use std::mem::swap;
 use num_integer::div_rem;
+
+use crate::arithmetic::{
+    addition::add_digit_slices_into,
+    multiplication::multiply_digit_slices_into,
+};
 
 /// The "base type" of the big digit
 ///
@@ -397,6 +403,26 @@ impl AsMut<[BigDigit]> for BigDigitVec {
     fn as_mut(&mut self) -> &mut [BigDigit] { self.0.as_mut() }
 }
 
+impl std::ops::Deref for BigDigitVec {
+    type Target = [BigDigit];
+    fn deref(&self) -> &Self::Target {
+        self.0.deref()
+    }
+}
+
+impl std::ops::Index<usize> for BigDigitVec {
+    type Output = BigDigit;
+    fn index(&self, index: usize) -> &Self::Output {
+        self.0.index(index)
+    }
+}
+
+impl std::ops::IndexMut<usize> for BigDigitVec {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        self.0.index_mut(index)
+    }
+}
+
 /// Custom wrapper for easily building BigDigitVec objects
 macro_rules! bigdigitbase_vec {
     [ capacity = $capacity:expr ] => {
@@ -416,6 +442,11 @@ macro_rules! bigdigit_vec {
     };
 }
 
+macro_rules! bigdigit_array {
+    [ $($e:expr),* ] => {
+        [ $(BigDigit($e)),* ]
+    };
+}
 
 macro_rules! impl_from_primitive {
     // special case for NonZero types
@@ -438,7 +469,7 @@ macro_rules! impl_from_primitive {
         impl From<$t> for BigDigitVec {
             fn from(mut num: $t) -> Self {
                 // cast is optimized away: https://rust.godbolt.org/z/rKjhEqbhf
-                if (num as usize) < (BIG_DIGIT_RADIX as usize) {
+                if (num as u128) < (BIG_DIGIT_RADIX as u128) {
                     return bigdigit_vec![num as BigDigitBase];
                 }
                 let mut result = bigdigit_vec![capacity = 3];
@@ -446,6 +477,10 @@ macro_rules! impl_from_primitive {
                     let (hi, lo) = div_rem(num, BIG_DIGIT_RADIX as $t);
                     result.push_raw_integer(lo as BigDigitBase);
                     num = hi;
+                }
+
+                if (num != 0) {
+                    result.push_raw_integer(num as BigDigitBase);
                 }
                 return result;
             }
@@ -476,26 +511,166 @@ impl From<BigDigit> for BigDigitVec {
     }
 }
 
-impl std::ops::Deref for BigDigitVec {
-    type Target = [BigDigit];
-    fn deref(&self) -> &Self::Target {
-        self.0.deref()
+/// Build from num-bigint
+impl From<num_bigint::BigInt> for BigDigitVec {
+    fn from(num: num_bigint::BigInt) -> Self {
+        use std::iter::FromIterator;
+
+        let digits = num.iter_u64_digits();
+        BigDigitVec::from_iter(digits)
     }
 }
 
 
-impl std::ops::Index<usize> for BigDigitVec {
-    type Output = BigDigit;
-    fn index(&self, index: usize) -> &Self::Output {
-        self.0.index(index)
+macro_rules! from_iter_radix_impl {
+    ($ty:ident, $val:ident, $radix:expr) => {{
+        let mut digits = $val.into_iter().peekable();
+        let digit0 = digits.next().unwrap_or(0);
+        if digits.peek() == None {
+            return BigDigitVec::from(digit0);
+        }
+
+        let capacity = 12;
+
+        // the contents of these vectors will be swapped, so we
+        // should initialize all with the same assumed capacity
+        let mut result = BigDigitVec::with_capacity(capacity);
+        let mut radix_shift_vec = BigDigitVec::with_capacity(capacity);
+        let mut digit_vec = BigDigitVec::with_capacity(capacity);
+        let mut tmp = BigDigitVec::with_capacity(capacity);
+
+        result.push_integer(digit0);
+        radix_shift_vec.push(BigDigit::one());
+
+        let radix_factor = $radix;
+
+        for digit in digits {
+            multiply_digit_slices_into(&radix_shift_vec, &radix_factor, &mut tmp);
+            swap(&mut radix_shift_vec, &mut tmp);
+
+            // expand next digit into our base by multiplying by radix_shift
+            digit_vec.replace_with(digit);
+            multiply_digit_slices_into(&digit_vec, &radix_shift_vec, &mut tmp);
+            swap(&mut digit_vec, &mut tmp);
+
+            // add_digit_slices_into
+            add_digit_slices_into(&result, &digit_vec, &mut tmp);
+            swap(&mut result, &mut tmp);
+        }
+        dbg!(digit_vec.capacity(), result.capacity(), tmp.capacity(), radix_shift_vec.capacity());
+
+        return result;
+    }};
+}
+
+/// Build from an iterator of little endian ordered u8 (base 256)
+impl std::iter::FromIterator<u8> for BigDigitVec {
+    fn from_iter<T: IntoIterator<Item=u8>>(iter: T) -> Self {
+        from_iter_radix_impl!(u8, iter, bigdigit_array![256])
     }
 }
 
-impl std::ops::IndexMut<usize> for BigDigitVec {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        self.0.index_mut(index)
+/// Build from an iterator of little endian ordered u16 (base 2**16)
+impl std::iter::FromIterator<u16> for BigDigitVec {
+    fn from_iter<T: IntoIterator<Item=u16>>(iter: T) -> Self {
+        from_iter_radix_impl!(u16, iter, bigdigit_array![65536])
     }
 }
+
+/// Build from an iterator of little endian ordered u32 (base 2**32)
+impl std::iter::FromIterator<u32> for BigDigitVec {
+    fn from_iter<T: IntoIterator<Item=u32>>(iter: T) -> Self {
+        from_iter_radix_impl!(u32, iter, bigdigit_array![294967296, 4])
+    }
+}
+
+/// Build from an iterator of little endian ordered u64 (base 2**64)
+impl std::iter::FromIterator<u64> for BigDigitVec {
+    fn from_iter<T: IntoIterator<Item=u64>>(iter: T) -> Self {
+        from_iter_radix_impl!(u64, iter, bigdigit_array![709551616, 446744073, 18]) // (u64::MAX as u128) + 1)
+    }
+}
+
+impl std::iter::FromIterator<u128> for BigDigitVec {
+    fn from_iter<T: IntoIterator<Item=u128>>(iter: T) -> Self {
+        from_iter_radix_impl!(u128, iter, bigdigit_array![768211456, 374607431, 938463463, 282366920, 340])
+    }
+}
+
+#[cfg(test)]
+mod test_from_iterator {
+    use super::*;
+    use std::iter::FromIterator;
+
+    include!("test_macros.rs");
+
+    macro_rules! test_impl {
+        ($t:ty: [ $($a:literal),* ] -> [ $($r:literal),+ ]) => {{
+            let digits = [$($a as $t),*];
+            let v = BigDigitVec::from_iter(digits);
+            assert_bigdecvec_eq!(v, [$($r),*]);
+        }}
+    }
+
+    #[test]
+    fn case_u8_11() {
+        test_impl!(u8: [11] -> [11]);
+    }
+
+    #[test]
+    fn case_u8_0_11() {
+        test_impl!(u8: [0, 11] -> [2816]);
+    }
+
+    #[test]
+    fn case_u8_20_0_90() {
+        test_impl!(u8: [20, 0, 90] -> [5898260]);
+    }
+
+    #[test]
+    fn case_u8_33_1_0_200() {
+        test_impl!(u8: [33, 1, 0, 200] -> [355443489, 3]);
+    }
+
+    #[test]
+    fn case_u64_19348() {
+        test_impl!(u64:[19348] -> [19348]);
+    }
+
+    #[test]
+    fn case_u64_30064771072() {
+        test_impl!(u64:[30064771072] -> [064771072, 30]);
+    }
+
+
+    #[test]
+    fn case_u64_292168128485179184529527303802857584559() {
+        test_impl!(u64:[10437139363181492143, 15838465981732763176] -> [857584559, 527303802, 179184529, 168128485, 292]);
+    }
+
+    #[test]
+    fn case_u64_33_1_0_7() {
+        test_impl!(u64:[33, 1, 0, 7] -> [951141921, 934855321, 664912734, 525962453, 765346850, 712147706, 43939]);
+    }
+
+    #[test]
+    fn case_u64_214312396452670092584246748532518639826000000000() {
+        test_impl!(u64: [5956997386804507648, 6516318922729427074, 629807528] ->[0, 518639826, 246748532, 670092584, 312396452, 214]);
+    }
+
+    #[test]
+    fn case_u64_31200072108630795005561881570000000000355287053() {
+        test_impl!(u64: [6732661218595913741, 5024132863428368696, 91688771] ->[355287053, 0, 561881570, 630795005, 200072108, 31]);
+    }
+
+    #[test]
+    fn case_u128_7820187036182534517127554804841109227374071() {
+        test_impl!(u128: [157961972447688275742951451643959903735, 22981]
+            -> [227374071, 804841109, 517127554, 36182534, 7820187]);
+    }
+
+}
+
 
 /// returns true if number is a power of ten (10^x)
 #[cfg(rustc_1_46)]
