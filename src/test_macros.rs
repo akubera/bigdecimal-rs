7c61c109a7d089770535e1d569325e9c7d3454b4
Andrew Kubera <andrewkubera@gmail.com>
2022-06-25 22:04:04 -0400

    Add test_macros.rs

---
+++ src/test_macros.rs
@@ -0,0 +1,54 @@
+// file to be included in tests modules
+
+macro_rules! apply {
+    ($func:ident, $a:literal, [ $($b:literal),* ]) => {{
+        let a = crate::bigdigit::build_big_digit($a);
+        let b = [ $( crate::bigdigit::build_big_digit($b) ),* ];
+        $func(&a, &b)
+    }};
+    ($func:ident, [ $($a:literal),* ], [ $($b:literal),* ]) => {{
+        let a = [ $( crate::bigdigit::build_big_digit($a) ),* ];
+        let b = [ $( crate::bigdigit::build_big_digit($b) ),* ];
+        $func(&a, &b)
+    }};
+}
+
+macro_rules! assert_bigdecvec_eq {
+    ( $v:ident, [ $($digits:literal),* ] ) => {
+        let expected = [ $( crate::bigdigit::build_big_digit($digits) ),* ];
+        assert_eq!($v.as_ref(), &expected);
+    };
+}
+
+macro_rules! call_func {
+    ( $func:ident, [ $($a:literal),* ] [ $($b:literal),* ] [ $($c:literal),* ]) => {{
+        let a = [ $( crate::bigdigit::build_big_digit($a) ),* ];
+        let b = [ $( crate::bigdigit::build_big_digit($b) ),* ];
+        let expected = [ $( crate::bigdigit::build_big_digit($c) ),* ];
+
+        $func(a.as_ref(), b.as_ref(), expected.as_ref());
+    }}
+}
+
+
+/// Implement test with different inputs depending on bigdigit radix
+///
+macro_rules! impl_test_for_radix {
+    ( $($rad:pat => [$($a:literal),*] [$($b:literal),*] == [$($c:literal),*]),* $(,)* ) => {{
+        let do_test = impl_test!();
+        match crate::bigdigit::BIG_DIGIT_RADIX {
+            $(
+                $rad => { call_func!(do_test, [$($a),*] [$($b),*] [$($c),*]) },
+            )*
+        };
+    }};
+    ( _ => $all:block, $($rad:pat => [$($a:literal),*] [$($b:literal),*] == [$($c:literal),*]),* $(,)*) => {{
+        let do_test = impl_test!();
+        match crate::bigdigit::BIG_DIGIT_RADIX {
+            $(
+                $rad => { call_func!(do_test, [$($a),*] [$($b),*] [$($c),*]) },
+            )*
+            _ => $all,
+        };
+    }};
+}
