675146300d881449279bc9eb22d0cd6c3b5b7370
Big Herc <bigherc18@gmail.com>
2023-02-24 01:45:06 +0100

    Feature: add support for `no_std` environments

---
+++ src/with_alloc.rs
@@ -0,0 +1,3 @@
+extern crate alloc;
+
+use alloc::{format, string, vec};
---
+++ src/with_std.rs
@@ -0,0 +1,4 @@
+use std::{cmp, convert, default, fmt, hash, num, ops, iter, str, string, i8, f32, f64};
+
+#[cfg(test)]
+use std::collections::hash_map::DefaultHasher;
