ec2a606eb91b9011f013f71853f023a77152f9be
Andrew Kubera <andrewkubera@gmail.com>
2023-07-11 02:58:48 -0400

    Impl Sub<{int}> and SubAssign<{int}>

--- src/impl_ops.rs
+++ src/impl_ops.rs
@@ -3,6 +3,8 @@
 use crate::BigDecimal;
 use crate::stdlib::ops::{
     Add, AddAssign,
+    Sub, SubAssign,
+    Neg,
 };
 
 macro_rules! impl_add_for_primitive {
@@ -79,3 +81,78 @@ impl_add_for_primitive!(i16);
 impl_add_for_primitive!(i32);
 impl_add_for_primitive!(i64);
 impl_add_for_primitive!(i128);
+
+
+macro_rules! impl_sub_for_primitive {
+    ($t:ty) => {
+        impl_sub_for_primitive!(IMPL:SUB $t);
+        impl_sub_for_primitive!(IMPL:SUB-ASSIGN $t);
+        impl_sub_for_primitive!(IMPL:SUB &$t);
+        impl_sub_for_primitive!(IMPL:SUB-ASSIGN &$t);
+    };
+    (IMPL:SUB $t:ty) => {
+        impl Sub<$t> for BigDecimal {
+            type Output = BigDecimal;
+
+            fn sub(mut self, rhs: $t) -> BigDecimal {
+                self -= rhs;
+                self
+            }
+        }
+
+        impl Sub<$t> for &BigDecimal {
+            type Output = BigDecimal;
+
+            fn sub(self, rhs: $t) -> BigDecimal {
+                let res = BigDecimal::from(rhs).neg();
+                res + self
+            }
+        }
+
+        impl Sub<BigDecimal> for $t {
+            type Output = BigDecimal;
+
+            fn sub(self, rhs: BigDecimal) -> BigDecimal {
+                rhs.neg() + self
+            }
+        }
+
+        impl Sub<&BigDecimal> for $t {
+            type Output = BigDecimal;
+
+            fn sub(self, rhs: &BigDecimal) -> BigDecimal {
+                rhs.neg() + self
+            }
+        }
+    };
+    (IMPL:SUB-ASSIGN &$t:ty) => {
+        impl SubAssign<&$t> for BigDecimal {
+            fn sub_assign(&mut self, rhs: &$t) {
+                *self -= *rhs;
+            }
+        }
+    };
+    (IMPL:SUB-ASSIGN $t:ty) => {
+        impl SubAssign<$t> for BigDecimal {
+            fn sub_assign(&mut self, rhs: $t) {
+                if self.scale == 0 {
+                    self.int_val -= rhs;
+                } else {
+                    *self -= BigDecimal::from(rhs);
+                }
+            }
+        }
+    };
+}
+
+
+impl_sub_for_primitive!(u8);
+impl_sub_for_primitive!(u16);
+impl_sub_for_primitive!(u32);
+impl_sub_for_primitive!(u64);
+impl_sub_for_primitive!(u128);
+impl_sub_for_primitive!(i8);
+impl_sub_for_primitive!(i16);
+impl_sub_for_primitive!(i32);
+impl_sub_for_primitive!(i64);
+impl_sub_for_primitive!(i128);
