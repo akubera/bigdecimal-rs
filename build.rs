4d4872fbe5c005247ce5597f7b81091679fff57b
Andrew Kubera <andrewkubera@gmail.com>
2023-08-20 13:16:03 -0400

    Move write_default_precision_file back to separate function

--- build.rs
+++ build.rs
@@ -7,11 +7,17 @@ fn main() {
     let ac = autocfg::new();
     ac.emit_rustc_version(1, 70);
 
+    let outdir: PathBuf = std::env::var_os("OUT_DIR").unwrap().into();
+    write_default_precision_file(&outdir);
+}
+
+
+/// Create default_precision.rs, containg definition of constant DEFAULT_PRECISION loaded in src/lib.rs
+fn write_default_precision_file(outdir: &PathBuf) {
     let env_var = env::var("RUST_BIGDECIMAL_DEFAULT_PRECISION").unwrap_or_else(|_| "100".to_owned());
     println!("cargo:rerun-if-env-changed=RUST_BIGDECIMAL_DEFAULT_PRECISION");
 
-    let outdir = std::env::var_os("OUT_DIR").unwrap();
-    let rust_file_path = PathBuf::from(outdir).join("default_precision.rs");
+    let rust_file_path = outdir.join("default_precision.rs");
 
     let default_prec: u32 = env_var
         .parse::<std::num::NonZeroU32>()
