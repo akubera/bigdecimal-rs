6a9a3d84ab527103b70e0fe85337fb06d3f32951
Andrew Kubera <andrewkubera@gmail.com>
2024-01-21 12:35:31 -0500

    Clean code and add docstrings to make-fmt-format-tests.py

--- scripts/make-fmt-format-tests.py
+++ scripts/make-fmt-format-tests.py
@@ -3,37 +3,9 @@
 # Generate Rust code given Decimals and formatting strings
 #
 
+import sys
 import textwrap
 from decimal import *
-
-
-FORMATS = [
-    "{}",
-    "{:+05.1}",
-    "{:.1}",
-    "{:.4}",
-    "{:4.1}",
-    "{:>4.3}",
-    "{:>4.4}",
-    "{:<4.1}",
-]
-
-# FORMATS = [
-#     "{:.4}",
-#     "{:.8}",
-# ]
-
-FORMATS = [
-    "{}",
-    "{:+05.7}",
-    "{:.3}",
-    "{:0.4}",
-    "{:4.1}",
-    "{:>8.3}",
-    "{:>8.4}",
-    "{:<8.1}",
-]
-
 
 # Decimal strings to the testing formats
 INPUTS = {
@@ -84,76 +56,72 @@ INPUTS = {
 
 
 def main():
+    from argparse import ArgumentParser, FileType
+
+    parser = ArgumentParser(description="Create test modules for src/impl_fmt.rs")
+    parser.add_argument("-o", "--output", nargs="?", default="-", type=FileType("w"))
+    args = parser.parse_args()
+
     for dec_str, formats in INPUTS.items():
-        dec = Decimal(dec_str)
-        mod_name = "dec_%s" % gen_name_from_dec(dec_str)
+        src = make_test_module_src(dec_str, formats)
+        args.output.write(src)
 
-        formats_and_outputs = [
-            (fmt, gen_name_from_fmt(fmt), fmt.format(dec)) for fmt in formats
-        ]
-
-        max_name_len = max(len(name) + 7 for _, name, _ in formats_and_outputs)
-
-        max_len_fmt = max(len(fmt) + 5 for fmt, _, _ in formats_and_outputs)
-
-        max_len_name_and_fmt = max(
-            len(fmt) + len(name) for fmt, name, _ in formats_and_outputs
-        )
-
-        lines = []
-        for fmt, name, value in formats_and_outputs:
-            spacer = " " * (max_len_name_and_fmt - (len(fmt) + len(name)) + 2)
-            # fmt = f'"{fmt}" =>'.rjust(max_len_fmt)
-            # fmt = fmt.rjust(max_len_fmt)
-            lines.append(f'impl_case!(fmt_{name}:{spacer}"{fmt}" => "{value}");')
-            # print('impl_case!(fmt_%s: "%s" => "%s");' % (name, fmt, ))
-
-        body = textwrap.indent("\n".join(lines), "    ")
-        text = textwrap.dedent(
-            f"""
-            mod {mod_name} {{
-                use super::*;
-
-                fn test_input() -> BigDecimal {{
-                    "{dec_str}".parse().unwrap()
-                }}
-
-            %s
-            }}"""
-        )
-
-        print(text % body)
-        continue
-
-        print(
-            "\n".join(
-                [
-                    f"mod {mod_name} {{",
-                    "use super::*;",
-                    f'fn test_input() -> BigDecimal {{ "{dec_str}".parse().unwrap() }}',
-                    body,
-                    "}",
-                ]
-            )
-        )
+    return 0
 
 
-# x = Decimal("1")
-x = 123456.0
+def make_test_module_src(dec_str: Decimal, formats: list[str]) -> str:
+    """
+    Return Rust module source which tests given decimal aginst given formats
+    """
+    dec = Decimal(dec_str)
+    mod_name = "dec_%s" % gen_name_from_dec(dec_str)
+
+    formats_and_outputs = [
+        (fmt, gen_name_from_fmt(fmt), fmt.format(dec)) for fmt in formats
+    ]
+
+    max_len_name_and_fmt = max(
+        len(fmt) + len(name) for fmt, name, _ in formats_and_outputs
+    )
+
+    lines = []
+    for fmt, name, value in formats_and_outputs:
+        spacer = " " * (max_len_name_and_fmt - (len(fmt) + len(name)) + 2)
+        lines.append(f'impl_case!(fmt_{name}:{spacer}"{fmt}" => "{value}");')
+
+    body = textwrap.indent("\n".join(lines), "    ")
+    text = textwrap.dedent(
+        f"""
+        mod {mod_name} {{
+            use super::*;
+
+            fn test_input() -> BigDecimal {{
+                "{dec_str}".parse().unwrap()
+            }}
+
+        %s
+        }}
+        """
+    )
+    return text % body
 
 
-def gen_name_from_dec(s: str) -> str:
-    return "".join(map_fmt_char_to_name_char(c) for c in s)
+def gen_name_from_dec(dec_str: str) -> str:
+    """Given decimal input string return valid function/module name"""
+    return "".join(map_fmt_char_to_name_char(c) for c in dec_str)
 
 
 def gen_name_from_fmt(fmt: str) -> str:
+    """Given decimal input string return valid function/module name"""
     if fmt == "{}":
         return "default"
     else:
+        assert fmt.startswith("{:") and fmt.endswith("}")
         return "".join(map_fmt_char_to_name_char(c) for c in fmt[2:-1])
 
 
 def map_fmt_char_to_name_char(c: str) -> str:
+    """Turn special characters into identifiers"""
     match c:
         case ".":
             return "d"
@@ -172,4 +140,4 @@ def map_fmt_char_to_name_char(c: str) -> str:
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
