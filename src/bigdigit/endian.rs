3dfdb160bd6ecb7e7eab1c6934b12cf421a5b254
Andrew Kubera <andrewkubera@gmail.com>
2025-05-03 17:01:30 -0400

    Add iteration methods to Endianness trait

--- src/bigdigit/endian.rs
+++ src/bigdigit/endian.rs
@@ -1,9 +1,23 @@
 //! Structs and traits for generic operations on big or little endian ints
 
+use crate::stdlib::fmt;
+use crate::stdlib::Vec;
+
 
 /// Trait to allow generic parameterization of significant digit ordering
-pub(crate) trait Endianness {}
+pub(crate) trait Endianness {
+    /// Iterate over digits in vec from least to most significance
+    fn into_iter<D>(digits: Vec<D>) -> impl Iterator<Item=D>;
 
+    /// Iterate in slice from least to most significance
+    fn iter_slice<D>(digits: &[D]) -> impl Iterator<Item=&D>;
+
+    /// Iterate over mut digits in slice from least to most significance
+    fn iter_slice_mut<D>(digits: &mut [D]) -> impl Iterator<Item=&mut D>;
+
+    /// Place given digit at the most-significant end of the vecor
+    fn push_significant_digit<D>(digits: &mut Vec<D>, d: D);
+}
 
 /// Empty struct indicating most-significant bigdigit first
 #[derive(Copy,Clone,Debug)]
@@ -13,6 +27,40 @@ pub(crate) struct BigEndian {}
 #[derive(Copy,Clone,Debug)]
 pub(crate) struct LittleEndian {}
 
-impl Endianness for BigEndian {}
 
-impl Endianness for LittleEndian {}
+impl Endianness for BigEndian {
+    fn into_iter<D>(digits: Vec<D>) -> impl Iterator<Item=D> {
+        digits.into_iter().rev()
+    }
+
+    fn iter_slice<D>(digits: &[D]) -> impl Iterator<Item=&D> {
+        digits.iter().rev()
+    }
+
+    fn iter_slice_mut<D>(digits: &mut [D]) -> impl Iterator<Item=&mut D>{
+        digits.iter_mut().rev()
+    }
+
+    fn push_significant_digit<D>(digits: &mut Vec<D>, d: D) {
+        digits.insert(0, d);
+    }
+}
+
+
+impl Endianness for LittleEndian {
+    fn into_iter<D>(digits: Vec<D>) -> impl Iterator<Item=D> {
+        digits.into_iter()
+    }
+
+    fn iter_slice<D>(digits: &[D]) -> impl Iterator<Item=&D> {
+        digits.iter()
+    }
+
+    fn iter_slice_mut<D>(digits: &mut [D]) -> impl Iterator<Item=&mut D> {
+        digits.iter_mut()
+    }
+
+    fn push_significant_digit<D>(digits: &mut Vec<D>, d: D) {
+        digits.push(d);
+    }
+}
