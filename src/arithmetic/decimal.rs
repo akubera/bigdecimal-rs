6e6b6ec6f19e1e9fc60df7275389c63fefe8df4c
Andrew Kubera <andrewkubera@gmail.com>
2025-05-04 14:01:52 -0400

    Replace decimal-shift lookup tables with hardcoded divides

--- src/arithmetic/decimal.rs
+++ src/arithmetic/decimal.rs
@@ -5,110 +5,73 @@
 //!
 #![allow(dead_code)]
 
-/// Lookup table used to divide u32 by a power of ten, using
-/// a multiply and a shift-right
-///
-/// Presumably faster than div?
-///
-static DEC_SHIFT_RIGHT_U32_MAGIC_NUMBERS: [(u64, usize); 10] = [
-    (1, 0),
-    (3435973837, 35),
-    (1374389535, 37),
-    (274877907, 38),
-    (3518437209, 45),
-    (5629499535, 49),
-    (1125899907, 50),
-    (1801439851, 54),
-    (1441151881, 57),
-    (4611686019, 62),
-];
 
-/// Shift u32 right by *n* decimal digits (panics if *n* is greater than 9)
-///
-///
-/// ```
-/// assert_eq!(dec_shift_right_u32(12345, 0), 12345);
-/// assert_eq!(dec_shift_right_u32(12345, 2), 123);
-/// assert_eq!(dec_shift_right_u32(12345, 6), 0);
-/// ```
-///
-pub fn dec_shift_right_u32_unchecked(x: u32, n: usize) -> u32 {
-    debug_assert!(n < DEC_SHIFT_RIGHT_U32_MAGIC_NUMBERS.len());
-
-    let (a, b) = DEC_SHIFT_RIGHT_U32_MAGIC_NUMBERS[n];
-    let y = x as u64 * a;
-    let result = (y >> b) as u32;
-    debug_assert_eq!(result, x / 10u32.pow(n as u32));
-    result
-}
-
-/// Shift u32 right by n decimal digit
-///
-/// ```
-/// assert_eq!(dec_shift_right_u32(12345, 0), 12345);
-/// assert_eq!(dec_shift_right_u32(12345, 2), 123);
-/// assert_eq!(dec_shift_right_u32(12345, 6), 0);
-/// ```
-///
+/// Shift u32 right by *n* decimal digits
 pub fn dec_shift_right_u32(x: u32, n: usize) -> u32 {
-    debug_assert!(n < DEC_SHIFT_RIGHT_U32_MAGIC_NUMBERS.len());
-
-    let (a, b) = match DEC_SHIFT_RIGHT_U32_MAGIC_NUMBERS.get(n) {
-        None => { return 0; }
-        Some(pair) => pair,
-    };
-    let y = x as u64 * a;
-    let result = (y >> b) as u32;
-    debug_assert_eq!(result, x / 10u32.pow(n as u32));
-    result
+    match n {
+        0 => x,
+        1 => x / 10,
+        2 => x / 100,
+        3 => x / 1000,
+        4 => x / 10_000,
+        5 => x / 100_000,
+        6 => x / 1000_000,
+        7 => x / 10_000_000,
+        8 => x / 100_000_000,
+        9 => x / 1000_000_000,
+        _ => 0,
+    }
 }
 
-/// Lookup table used to divide u64 by a power of ten, using
-/// a multiply and a shift-right
-static DEC_SHIFT_RIGHT_U64_MAGIC_NUMBERS: [(u128, usize); 20] = [
-    (1, 0),
-    (14757395258967641293, 67),
-    (23611832414348226069, 71),
-    (18889465931478580855, 74),
-    (3777893186295716171, 75),
-    (24178516392292583495, 81),
-    (4835703278458516699, 82),
-    (15474250491067253437, 87),
-    (12379400392853802749, 90),
-    (19807040628566084399, 94),
-    (15845632502852867519, 97),
-    (12676506002282294015, 100),
-    (2535301200456458803, 101),
-    (4056481920730334085, 105),
-    (811296384146066817, 106),
-    (20769187434139310515, 114),
-    (4153837486827862103, 115),
-    (26584559915698317459, 121),
-    (21267647932558653967, 124),
-    (8507059173023461587, 126),
-];
-
-/// Shift u64 right by *n* decimal digits (panics if *n* is greater than 19)
-pub fn dec_shift_right_u64_unchecked(x: u64, n: usize) -> u64 {
-    debug_assert!(n < DEC_SHIFT_RIGHT_U64_MAGIC_NUMBERS.len());
-
-    let (a, b) = DEC_SHIFT_RIGHT_U64_MAGIC_NUMBERS[n];
-    let y = x as u128 * a;
-    let result = (y >> b) as u64;
-    debug_assert_eq!(result, x / 10u64.pow(n as u32));
-    result
-}
 
 /// Shift u64 right by *n* decimal digits
 pub fn dec_shift_right_u64(x: u64, n: usize) -> u64 {
-    debug_assert!(n < DEC_SHIFT_RIGHT_U64_MAGIC_NUMBERS.len());
+    match n {
+        0 => x,
+        1 => x / 10,
+        2 => x / 100,
+        3 => x / 1000,
+        4 => x / 10_000,
+        5 => x / 100_000,
+        6 => x / 1000_000,
+        7 => x / 10_000_000,
+        8 => x / 100_000_000,
+        9 => x / 1000_000_000,
+        10 => x / 10_000_000_000,
+        11 => x / 100_000_000_000,
+        12 => x / 1000_000_000_000,
+        13 => x / 10_000_000_000_000,
+        14 => x / 100_000_000_000_000,
+        15 => x / 1000_000_000_000_000,
+        16 => x / 10_000_000_000_000_000,
+        17 => x / 100_000_000_000_000_000,
+        18 => x / 1000_000_000_000_000_000,
+        19 => x / 10_000_000_000_000_000_000,
+        _ => 0,
+    }
+}
 
-    let (a, b) = match DEC_SHIFT_RIGHT_U64_MAGIC_NUMBERS.get(n) {
-        None => { return 0; }
-        Some(pair) => pair,
-    };
-    let y = x as u128 * a;
-    let result = (y >> b) as u64;
-    debug_assert_eq!(result, x / 10u64.pow(n as u32));
-    result
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    mod dec_shift_right_u32 {
+        use super::*;
+
+        macro_rules! impl_case {
+            ($name:ident: ($n:literal, $s:literal) => $expected:literal) => {
+                #[test]
+                fn $name() {
+                    assert_eq!(dec_shift_right_u32($n, $s), $expected);
+                    assert_eq!(dec_shift_right_u32($n, $s), $expected);
+                }
+            };
+        }
+
+        impl_case!(case_12345_0: (12345, 0) => 12345 );
+        impl_case!(case_12345_2: (12345, 2) => 123 );
+        impl_case!(case_12345_6: (12345, 6) => 0 );
+        impl_case!(case_12345_7: (12345, 7) => 0 );
+    }
 }
